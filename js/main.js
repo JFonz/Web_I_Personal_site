881ee30840e1cfe087be41cd6a3e16575be06aaa
Jasen Fonnesbeck <jfsoccerman@hotmail.com>
2020-11-12 11:08:34 -0700

    condensed js with switch

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
-function showh(){
-    document.getElementById("home").style.display = "block";
-    document.getElementById("members").style.display = "none";
-    document.getElementById("videos").style.display = "none";
-}
+function showClicked(clicked){
+    var home = document.getElementById("home");
+    var members = document.getElementById("members");
+    var videos = document.getElementById("videos");
 
-function showm(){
-    document.getElementById("home").style.display = "none";
-    document.getElementById("members").style.display = "block";
-    document.getElementById("videos").style.display = "none";
-}
-
-function showv(){
-    document.getElementById("home").style.display = "none";
-    document.getElementById("members").style.display = "none";
-    document.getElementById("videos").style.display = "block";
+    switch(clicked){
+        case "btnh":
+            home.style.display = "block";
+            members.style.display = "none";
+            videos.style.display = "none";
+            break;
+        case "btnm":
+            home.style.display = "none";
+            members.style.display = "block";
+            videos.style.display = "none";
+            break;
+        case "btnv":
+            home.style.display = "none";
+            members.style.display = "none";
+            videos.style.display = "block";
+            break;
+        default:
+            home.style.display = "block";
+            members.style.display = "none";
+            videos.style.display = "none";
+            break;
+    }
 }
 
 window.addEventListener("DOMContentLoaded", function(){
     //add event listeners
-    document.getElementById("btnh").addEventListener("click", showh);
-    document.getElementById("btnm").addEventListener("click", showm);
-    document.getElementById("btnv").addEventListener("click", showv);
+    document.getElementById("btnh").addEventListener("click", function(){showClicked("btnh");});
+    document.getElementById("btnm").addEventListener("click", function(){showClicked("btnm");});
+    document.getElementById("btnv").addEventListener("click", function(){showClicked("btnv");});
 });
