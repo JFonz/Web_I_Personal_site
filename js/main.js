cc7f4a68ec27d02f49df4be6f990d15d55b0d08d
Jasen Fonnesbeck <jfsoccerman@hotmail.com>
2020-11-23 12:10:43 -0700

    added visitor form and styling

--- js/main.js
+++ js/main.js
@@ -2,27 +2,39 @@ function showClicked(clicked){
     var $home = $("#home");
     var $members = $("#members");
     var $videos = $("#videos");
+    var $log = $("#log-visit");
 
     switch(clicked){
         case "btnh":
             $home.css("display", "block");
             $members.css("display", "none");
             $videos.css("display", "none");
+            $log.css("display", "none");
             break;
         case "btnm":
             $home.css("display", "none");
             $members.css("display", "block");
             $videos.css("display", "none");
+            $log.css("display", "none");
             break;
         case "btnv":
             $home.css("display", "none");
             $members.css("display", "none");
             $videos.css("display", "block");
+            $log.css("display", "none");
+            break;
+        case "btnlv":
+            $home.css("display", "none");
+            $members.css("display", "none");
+            $videos.css("display", "none");
+            $log.css("display", "block");
+
             break;
         default:
             $home.css("display", "block");
             $members.css("display", "none");
             $videos.css("display", "none");
+            $log.css("display", "none");
             break;
     }
 }
@@ -31,4 +43,5 @@ $(document).ready(function() {
     $("#btnh").click(function(){showClicked("btnh")});
     $("#btnm").click(function(){showClicked("btnm")});
     $("#btnv").click(function(){showClicked("btnv")});
+    $("#btnlv").click(function(){showClicked("btnlv")});
 })
